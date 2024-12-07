a04fbcfb532e47e532412ce739e76dafc1c7b530
Martin <martin.dunisch@gmail.com>
2024-12-07 21:44:48 +0100

    day 3, part 2

--- 3/solution.ts
+++ 3/solution.ts
@@ -10,6 +10,25 @@ export function part1(input: string) {
 
 export function part2(input: string) {
 
-    return 0;
+    const searchExp = /mul\(([0-9]{1,3}),([0-9]{1,3})\)|don't\(\)|do\(\)/g;
 
+    let result = 0;
+    let multiply = true;
+
+    [...input.matchAll(searchExp)].forEach(([ operator, factor1, factor2]) => {
+        if (operator === "don't()") {
+            multiply = false;
+        }
+
+        if (operator === "do()") {
+            multiply = true;
+        }
+
+        if (multiply && operator.startsWith('mul')) {
+            result += parseInt(factor1) * parseInt(factor2);
+        }
+    });
+
+    return result;
 }
+
--- 3/test.ts
+++ 3/test.ts
@@ -11,5 +11,8 @@ Deno.test("part 1", () => {
 
 
 Deno.test("part 2", () => {
-  assertEquals(part2(input), 48);
+  
+  const input2 = `xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))`;
+  
+  assertEquals(part2(input2), 48);
 });
