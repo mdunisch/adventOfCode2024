29ba00e4e7d81af5be463670d6146cd57b14f385
Martin <martin.dunisch@gmail.com>
2024-12-07 21:01:23 +0100

    day 2, part 2

--- 2/solution.ts
+++ 2/solution.ts
@@ -4,28 +4,56 @@ function isMonotonic(input: number[]): boolean {
         input.every((v, i) => i === 0 || v >= input[i - 1])
 }
 
+function isGraduallySafe(input: number[]): boolean {
+    return input.every((value, i) => {
+        if (i === 0) {
+            return true
+        }
+        const diff = Math.abs(value - input[i - 1]);
+        return (diff >= 1 && diff <= 3);
+    })
+}
+
+function arrayWithout(input: number[], position: number) {
+    return input.slice(0, position).concat(input.slice(position + 1));
+}
+
 
 export function part1(input: string) {
 
     const inputAsNumbers = input.split('\n').map(i => i.split(' ').map(i => parseInt(i)));
 
-    const validLines = inputAsNumbers.filter(line => {
-
-        const isGraduallySafe = line.every((value, i) => {
-            if (i === 0) {
-                return true
-            }
-            const diff = Math.abs(value - line[i - 1]);
-            return (diff >= 1 && diff <= 3);
-        });
-
-        return isGraduallySafe && isMonotonic(line);
-    });
+    const validLines = inputAsNumbers.filter(line => isGraduallySafe(line) && isMonotonic(line));
 
     return validLines.length;
 
 }
 
 export function part2(input: string) {
-    return 0;
+
+    const inputAsNumbers = input.split('\n').map(i => i.split(' ').map(i => parseInt(i)));
+
+    const validLines = inputAsNumbers.filter(line => {
+
+        if (isGraduallySafe(line) && isMonotonic(line)) {
+            return true;
+        }
+
+        let vaild = false;
+
+        for (let i = 0; i < line.length; i++) {
+            const inputToCheck = arrayWithout(line, i);
+            vaild = isGraduallySafe(inputToCheck) && isMonotonic(inputToCheck);
+
+            if (vaild) {
+                break;
+            }
+        }
+
+        return vaild;
+
+    });
+
+    return validLines.length;
+
 }
